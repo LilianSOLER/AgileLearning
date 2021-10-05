73c4854ce02055f7cffc6345404274a58a14e911
Lilian Soler <soler.lilian64@gmail.com>
2021-10-05 16:32:41 +0200

    Sprint prog.basics, Task1 completed

---
+++ workspace/hello.greetings/src/hello/greetings/Greetings.java
@@ -0,0 +1,14 @@
+package hello.greetings;
+
+public class Greetings {
+	 public static void main(String[] args) {
+		    System.out.print("Greetings");
+		    int i=0;
+		    while (i < args.length) {
+		      System.out.printf(" %s", args[i]);
+		      i = i + 1;
+		    }
+		    System.out.println("!");
+		    return;
+		  }
+}
